d58400aa3044b82f7f5016969861ecd21aace0e7
Michal Pielka <mpielka726@gmail.com>
2026-03-08 21:46:10 +0100

    feat: read raw sql dumps

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+const PAGELINKS_SQL_PATH: &str = "../wiki-sql/simplewiki-latest-pagelinks.sql";
+const PAGE_SQL_PATH: &str = "../wiki-sql/simplewiki-latest-page.sql";
+const LINKTARGET_SQL_PATH: &str = "../wiki-sql/simplewiki-latest-linktarget.sql";
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let pagelinks_bytes = std::fs::read(PAGELINKS_SQL_PATH)?;
+    let page_bytes = std::fs::read(PAGE_SQL_PATH)?;
+    let linktarget_bytes = std::fs::read(LINKTARGET_SQL_PATH)?;
+
+    Ok(())
 }
