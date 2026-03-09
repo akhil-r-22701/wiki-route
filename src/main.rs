677df29fb1fe4e76347085b5c07f66123984b4e0
Michal Pielka <mpielka726@gmail.com>
2026-03-09 03:43:52 +0100

    feat: add pagelinks parser

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,18 @@
+use std::io::BufReader;
+use std::fs::File;
+
+use wiki_route::parser;
+
 const PAGELINKS_SQL_PATH: &str = "../wiki-sql/simplewiki-latest-pagelinks.sql";
 const PAGE_SQL_PATH: &str = "../wiki-sql/simplewiki-latest-page.sql";
 const LINKTARGET_SQL_PATH: &str = "../wiki-sql/simplewiki-latest-linktarget.sql";
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let pagelinks_bytes = std::fs::read(PAGELINKS_SQL_PATH)?;
-    let page_bytes = std::fs::read(PAGE_SQL_PATH)?;
-    let linktarget_bytes = std::fs::read(LINKTARGET_SQL_PATH)?;
+    let pagelinks_file = File::open(PAGELINKS_SQL_PATH)?;
+    let pagelinks_reader = BufReader::new(pagelinks_file);
+
+    let pagelinks = parser::parse_pagelinks(pagelinks_reader)?;
+    println!("Parsed {} pagelink", pagelinks.len());
 
     Ok(())
 }
---
+++ src/parser.rs
@@ -0,0 +1,45 @@
+use std::io::BufRead;
+
+pub struct PageId(pub u32);
+pub struct LinkTargetId(pub u64);
+
+const INSERT_PREFIX: &str = "INSERT INTO `pagelinks` VALUES ";
+
+pub fn parse_pagelinks<R: BufRead>(reader: R) -> Result<Vec<(PageId, LinkTargetId)>, std::io::Error> {
+    let mut pagelinks = Vec::new();
+
+    for line in reader.lines() {
+        let line = line?;
+
+        // Skip lines not starting with INSERT_PREFIX
+        let Some(rest) = line.strip_prefix(INSERT_PREFIX) else {
+            continue;
+        };
+
+        let rest = rest.strip_suffix(';').unwrap_or(rest);
+
+        for tuple in rest.split("),(") {
+            let tuple = tuple.trim_matches(|c| c == '(' || c == ')');
+            let mut parts = tuple.splitn(3, ',');
+
+            let (Some(from), Some(from_ns), Some(target_id)) =
+                (parts.next(), parts.next(), parts.next())
+            else {
+                continue;
+            };
+
+            if from_ns != "0" {
+                continue;
+            }
+
+            let (Ok(from), Ok(target_id)) = (from.parse::<u32>(), target_id.parse::<u64>())
+            else {
+                continue;
+            };
+
+            pagelinks.push((PageId(from), LinkTargetId(target_id)));
+        }
+    }
+
+    Ok(pagelinks)
+}
