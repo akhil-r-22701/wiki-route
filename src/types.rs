6923d03432bb9e25e768ef23d0c7a59f3a9b2342
Michal Pielka <mpielka726@gmail.com>
2026-03-09 04:55:01 +0100

    feat: add Graph type

--- src/types.rs
+++ src/types.rs
@@ -1,3 +1,8 @@
 pub type PageId = u32;
 pub type LinkTargetId = u64;
 pub type PageTitle = String;
+
+// Graph adjacency list
+// We use Vec instead of Hashmap to save time on hashing
+// graph[page_id] = [neigh1_page_id, neigh2_page_id, ...]
+pub type Graph = Vec<Vec<PageId>>;
