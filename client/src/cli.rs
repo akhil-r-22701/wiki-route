6b77e716ab462254916e02a9df0e0f523ebd1cd6
Michal Pielka <mpielka726@gmail.com>
2026-03-25 00:47:13 +0100

    feat: add client cli

--- client/src/cli.rs
+++ client/src/cli.rs
@@ -6,6 +6,12 @@ use clap::Parser;
 #[command(name = "wiki-route")]
 #[command(about = "Wikipedia shortest-path client")]
 pub struct Cli {
+    /// Source Wikipedia page title
+    pub from: String,
+
+    /// Target Wikipedia page title
+    pub to: String,
+
     /// Unix socket path to connect to
     #[arg(long, default_value = "/tmp/wiki-route.sock")]
     pub socket: PathBuf,
--- client/src/main.rs
+++ client/src/main.rs
@@ -1,12 +1,28 @@
 mod cli;
 
+use std::io::{BufRead, BufReader, Write};
+use std::os::unix::net::UnixStream;
+
 use clap::Parser;
 use cli::Cli;
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let cli = Cli::parse();
 
-    // TODO: connect to cli.socket, send query, print result
-    println!("Connecting to {}...", cli.socket.display());
-    todo!()
+    let mut stream = UnixStream::connect(&cli.socket)?;
+
+    writeln!(stream, "{}", cli.from)?;
+    writeln!(stream, "{}", cli.to)?;
+    stream.flush()?;
+
+    let reader = BufReader::new(&stream);
+    for line in reader.lines() {
+        let line = line?;
+        if line.is_empty() {
+            break;
+        }
+        println!("{}", line);
+    }
+
+    Ok(())
 }
